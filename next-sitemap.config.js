2a2a24dac97c1d256942633b38eb6914211404a0
palajo <palajoleg@gmail.com>
2022-09-13 11:19:30 +0300

    [update] added sitemap, pixel to head

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://ak-investment.pl',
+  generateRobotsTxt: true,
+}
+
+export default config;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,25 +2,7 @@ import '../styles/styles.scss';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('2136501293194812')
-        ReactPixel.pageView()
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
-
   return <Component {...pageProps} />
 }
 
