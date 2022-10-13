b9962fb9b23ee0c1d2749c53150b098a0002c5cf
palajo <palajoleg@gmail.com>
2022-10-13 22:31:32 +0300

    [update] pixel upd

--- src/components/facebook/Pixel.js
+++ src/components/facebook/Pixel.js
@@ -5,26 +5,26 @@ export default function Pixel() {
     <>
       <script dangerouslySetInnerHTML={{
         __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
           n.queue=[];t=b.createElement(e);t.async=!0;
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '2136501293194812');
-        fbq('track', 'PageView');
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1053179998705850');
+          fbq('track', 'PageView');
         `
       }}
       />
       <noscript dangerouslySetInnerHTML={{
         __html: `
           <img height="1" width="1" style="display:none"
-              src="https://www.facebook.com/tr?id=2136501293194812&ev=PageView&noscript=1"
+              src="https://www.facebook.com/tr?id=1053179998705850&ev=PageView&noscript=1"
           />
         `
-      }}
+        }}
       />
     </>
   )
